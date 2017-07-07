2c4c7fa30f6efaa89d3d3b21abaac551b212fef6
shiraiwa <shiraiwa@psfc.mit.edu>
2017-07-06 20:09:11 -0400

    compile passed on engaging

--- setup_templates/setup_macport.py
+++ setup_templates/setup_macport.py
@@ -3,13 +3,24 @@
 """
 setup.py file for SWIG example
 """
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+import sys
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
 print('building mumps solver interface')
 
 ## first load variables from PyMFEM_ROOT/setup_local.py
-import sys
-import os
 root =  os.path.abspath(os.path.join(os.path.realpath(__file__),
-                                     '..', '..', '..', '..'))
+                                     '..', 'build'))
+print root
 sys.path.insert(0, root)
 from  setup_local import *
 
@@ -18,7 +29,8 @@ from distutils      import sysconfig
 
 modules= ["mumps_solve", "hypre_to_mumps"]
 
-sources = {name: [name + "_wrap.cxx"] for name in modules}
+sdir = "petram/ext/mumps/"
+sources = {name: [sdir + name + "_wrap.cxx"] for name in modules}
 
 proxy_names = {name: '_'+name for name in modules}
 
@@ -35,17 +47,35 @@ libraries    = [hyprelib, pordlib, parmetislib, metis5lib, scalapacklib, mpilib,
 ext_modules = [Extension(proxy_names[name],
                         sources=sources[name],
                         extra_compile_args = ['-DSWIG_TYPE_TABLE=PyMFEM'],   
-                        extra_link_args = extra_link_args + [name+'.a', "-Wl,-no_compact_unwind"],
+                        extra_link_args = extra_link_args + [sdir + name+'.a', "-Wl,-no_compact_unwind"],
                         include_dirs = include_dirs,
                         library_dirs = library_dirs,
                         libraries = libraries )
               for name in modules]
 
 
-setup (name = 'mumps_c_example',
+setup (name = 'PetraM_MUMPS',
+       url='https://github.com/piScope/PetraM',       
        version = '0.1',
-       author      = "S.Shiraiwa",
-       description = """C_EXAMPLE wrapper""",
+       description = 'PetraM MUMPS interface', 
+       long_description=long_description,       
+       author      = "S. Shiraiwa",
+       author_email='shiraiwa@psfc.mit.edu',
+       license='GNUv3',
+       
+       classifiers=[
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 2.7',
+       ],
+
+       keywords='MFEM physics',
+       packages=find_packages(),
        ext_modules = ext_modules,
-       py_modules = modules, 
-       )
+       py_modules = modules,
+)
--- setup_templates/setup_mit_engaging.py
+++ setup_templates/setup_mit_engaging.py
@@ -3,13 +3,24 @@
 """
 setup.py file for SWIG example
 """
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+import sys
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
 print('building mumps solver interface')
 
 ## first load variables from PyMFEM_ROOT/setup_local.py
-import sys
-import os
 root =  os.path.abspath(os.path.join(os.path.realpath(__file__),
-                                     '..', '..', '..', '..'))
+                                     '..', 'build'))
+print root
 sys.path.insert(0, root)
 from  setup_local import *
 
@@ -18,7 +29,8 @@ from distutils      import sysconfig
 
 modules= ["mumps_solve", "hypre_to_mumps"]
 
-sources = {name: [name + "_wrap.cxx"] for name in modules}
+sdir = "petram/ext/mumps/"
+sources = {name: [sdir + name + "_wrap.cxx"] for name in modules}
 
 proxy_names = {name: '_'+name for name in modules}
 
@@ -27,8 +39,11 @@ mumps_link_args =  [libporda, mumpscommonliba, dmumpsliba, smumpsliba, cmumpslib
 
 include_dirs = [hypreincdir, mfemincdir, mpichincdir, numpyincdir,
                 mpi4pyincdir, numpyincdir, mumpsincdir1, mumpsincdir2]
-library_dirs = [hyprelnkdir, pordlnkdir, metis5lnkdir, parmetislnkdir, scalapacklnkdir,  mpichlnkdir]
+#library_dirs = [mfemlnkdir, hyprelnkdir, pordlnkdir, parmetislnkdir, mpichlnkdir]
+#libraries    = [mfemlib, hyprelib, pordlib, parmetislib, scalapacklib, mpilib, blaslib, pthreadlib]
+library_dirs = [hyprelnkdir, pordlnkdir, metis5lnkdir, parmetislnkdir, mpichlnkdir]
 libraries    = [hyprelib, pordlib, parmetislib, metis5lib, scalapacklib, mpilib, blaslib, pthreadlib]
+
 
 library_dirs = [x for x in library_dirs if len(x) != 0]
 libraries = [x for x in libraries if len(x) != 0]
@@ -37,9 +52,9 @@ ext_modules = []
 
 for kk, name in enumerate(modules):
    if kk == 0:
-       extra_link_args = mumps_link_args + [name+'.a']
+       extra_link_args = mumps_link_args + [sdir + name+'.a']
    else:
-       extra_link_args = [name+'.a']
+       extra_link_args = [sdir + name+'.a']
 
    extra_text = [x for x in
                  ['-Wl', whole_archive] +  extra_link_args + 
@@ -57,12 +72,28 @@ for kk, name in enumerate(modules):
                         library_dirs = library_dirs,
                         libraries = libraries ))
 
-
-
-setup (name = 'mumps_c_example',
+setup (name = 'PetraM_MUMPS',
+       url='https://github.com/piScope/PetraM',       
        version = '0.1',
-       author      = "S.Shiraiwa",
-       description = """C_EXAMPLE wrapper""",
+       description = 'PetraM MUMPS interface', 
+       long_description=long_description,       
+       author      = "S. Shiraiwa",
+       author_email='shiraiwa@psfc.mit.edu',
+       license='GNUv3',
+       
+       classifiers=[
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 2.7',
+       ],
+
+       keywords='MFEM physics',
+       packages=find_packages(),
        ext_modules = ext_modules,
-       py_modules = modules, 
-       )
+       py_modules = modules,
+)
